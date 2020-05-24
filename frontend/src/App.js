224f8eb38deb299f80751c37ff4243d880623d36
charles mancuso <charlesmancuso66@yahoo.com>
2020-05-23 20:50:40 -0700

    Add render and export

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -84,3 +84,26 @@ class App extends Component {
       </li>
     ));
   };
+
+  render() {
+    return (
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+        <div className="row ">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="">
+                <button className="btn btn-primary">Add task</button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+export default App;
