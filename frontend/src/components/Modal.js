6e0319e859318c9c8dffade22c892e4441d8a5ba
charles mancuso <charlesmancuso66@yahoo.com>
2020-05-26 17:20:15 -0700

    Add custommodal class

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -10,3 +10,19 @@ import {
   Input,
   Label
 } from "reactstrap";
+
+export default class CustomModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: this.props.activeItem
+    };
+  }
+  handleChange = e => {
+    let { name, value } = e.target;
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
+    }
+    const activeItem = { ...this.state.activeItem, [name]: value };
+    this.setState({ activeItem });
+  };
